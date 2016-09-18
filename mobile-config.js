14923de2843e396b3022ff3be594f68abc5e7a20
Paul Laskowski <pelaskowski@gmail.com>
2016-09-18 13:19:15 -0700

    fixed uri

--- mobile-config.js
+++ mobile-config.js
@@ -4,7 +4,7 @@ App.info({
   description: 'A game that requires concentration',
   author: 'Paul Laskowski',
   email: 'paul@warmbowski.com',
-  website: 'https://gridmatch.heroku.com'
+  website: 'https://gridmatch.herokuapp.com'
 });
 
 App.setPreference('Orientation', 'default');
